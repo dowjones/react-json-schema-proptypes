cc114adeeaa7750a933faaa4354c90ca895d36a1
Stuart Harris <stuart.harris@red-badger.com>
2015-11-19 16:28:42 -0600

    switch to babel-core. more es6-ing. added hasOwnProperty check

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-import AJV from 'ajv';
+import AJV from 'ajv'
 const ajv = AJV();
 
-function getPropError(propName, errors) {
-  for(var i = 0; i < errors.length; i++) {
-    if(errors[i].dataPath === '.' + propName)
-      return errors[i];
+export default function(schema) {
+  if (typeof schema !== 'object') {
+    throw new TypeError('Schema must be of type \'object\'');
   }
-
-  return null;
-}
-
-module.exports = function(schema) {
-  if (typeof schema !== "object") throw new TypeError("Schema must be of type 'object'");
-  if (schema.type !== "object") throw new Error("Schema must define an object type (currently: " + schema.type + ")");
+  if (schema.type !== 'object') {
+    throw new Error(`Schema must define an object type (currently: ${schema.type})`);
+  }
 
   // Creates a new prototype chain with the schema applied to it.
   // This hides the schema from React but exposes the validators correctly.
@@ -24,15 +19,17 @@ module.exports = function(schema) {
     const validate = ajv.compile(schema);
 
     for (let prop in schema.properties) {
-      propTypes[prop] = function(props, propName, componentName) {
-        const valid = validate(props);
-        if (valid) return null;
+      if (schema.properties.hasOwnProperty(prop)) {
+        propTypes[prop] = function(props, propName, componentName) {
+          const valid = validate(props);
+          if (valid) return null;
 
-        const propError = getPropError(propName, validate.errors);
-        if (!propError) return null;
+          const propError = validate.errors.find(e => e.dataPath === `.${propName}`);
+          if (!propError) return null;
 
-        return new Error("'" + propName + "' " + propError.message + ', found ' + JSON.stringify(props[propName]) + ' instead. Check propTypes of component ' + componentName);
-      };
+          return new Error(`'${propName}' ${propError.message}, found ${JSON.stringify(props[propName])} instead. Check propTypes of component ${componentName}`);
+        };
+      }
     }
   }
 

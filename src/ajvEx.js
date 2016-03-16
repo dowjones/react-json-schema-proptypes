f389beea7f7f9ec4eb9e3fb217c419556b55f682
pandah <hrusikesh.panda@dowjones.com>
2016-03-16 18:14:12 -0400

    Make eslint happy

--- src/ajvEx.js
+++ src/ajvEx.js
@@ -5,9 +5,9 @@ function safeEscapeQuotes(str) {
   return str.replace(/\\([\s\S])|(')/g,"\\$1$2").replace(/\\([\s\S])|(")/g,"\\$1$2"); // escape only if not escaped already
 }
 
-ajv.addKeyword('deprecated', { inline: function (it, keyword, schema, parentSchema){
+ajv.addKeyword('deprecated', { inline: function (it, keyword, schema){
   var op = schema ? `console.warn('Property "${it.schemaPath.replace(/^\.properties\./, '')}" was deprecated on: ${schema.deprecatedOn}. Recommended alternative is: "${safeEscapeQuotes(schema.description)}".')` : '';
-  return `${op} || 1`
-}})
+  return `${op} || 1`;
+}});
 
-export default ajv
+export default ajv;
